c63770c6e866eb5909760b1f9b329d80c66c3cca
Serquand <=>
2022-12-08 22:38:52 +0100

    Added some packages and creating some user endpoint

--- Controllers/user.route.js
+++ Controllers/user.route.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+router.post("/signin", (req, res) => {
+    
+})
 
+router.post("/register", (req, res) => {
+
+})
+
+router.patch("/addInfo", (req, res) => {
+
+})
 
 module.exports = router;
