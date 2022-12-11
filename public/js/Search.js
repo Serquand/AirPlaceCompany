362c708cd7cc8ff433e6b61efefa166ba43dc381
Serquand <=>
2022-12-11 15:48:30 +0100

    Search

--- public/js/Search.js
+++ public/js/Search.js
@@ -1,4 +1,5 @@
 const container = document.querySelectorAll(".booking-form-component");
+const tickets = document.querySelectorAll("#buy-tickets > div")
 
 container[0].querySelector("input").addEventListener("input", (event) => {
     let pos = 0;
@@ -28,7 +29,7 @@ container[1].querySelector("input").addEventListener("input", (event) => {
             continue
         }
         
-        if(airport.innerText.split("(")[0].trim().toUpperCase().startsWith(event.target.value.toUpperCase())) {
+        if(airport.innerText.split("(")[0].trim().toUpperCase().startsWith(event.target.value.toUpperCase()) || pos <= 5) {
             airport.style.display = "block";
             airport.style.transform = `translateY(${++pos}00%)`
             continue;
@@ -38,10 +39,13 @@ container[1].querySelector("input").addEventListener("input", (event) => {
     }
 });
 
+for(let i = 0; i < Math.min(10, tickets.length); i++) tickets[i].style.display = "flex";
+
 const allArrival = container[1].querySelectorAll(".airport-choice");
 allArrival.forEach((airport) => {
     airport.addEventListener("click", (event) => {
-        container[1].querySelector("input").value = event.target.innerText;
+        if(event.target.innerText.includes("(")) container[1].querySelector("input").value = event.target.innerText;
+        else container[1].querySelector("input").value = event.target.parentNode.innerText;
         allArrival.forEach((airport) => {
             airport.style.display = "none";
         })  
@@ -51,13 +55,33 @@ allArrival.forEach((airport) => {
 const allDeparture = container[0].querySelectorAll(".airport-choice");
 allDeparture.forEach((airport) => {
     airport.addEventListener("click", (event) => {
-        container[0].querySelector("input").value = event.target.innerText;
+        if(event.target.innerText.includes("(")) container[0].querySelector("input").value = event.target.innerText;
+        else container[0].querySelector("input").value = event.target.parentNode.innerText;
         allDeparture.forEach((airport) => {
             airport.style.display = "none";
         })  
     })
 })
 
-document.querySelector("#button-search-flight").addEventListener("click", (event) => {
-    
+document.querySelector("#button-search-flight").addEventListener("click", () => {
+    const airportArrival = document.getElementById("arrival-aiport-choice").value
+    const airportDeparture = document.getElementById("departure-airport-choice").value
+    let date = document.getElementById("date-choice").value.split("-")
+    date = date[2] + "/" + date[1] + "/" + date[0]
+    let find = false 
+
+    tickets.forEach(ticket => {
+        const ticketArrival = ticket.querySelector(".dest > :last-child").innerText
+        const ticketDeparture = ticket.querySelector(".dest > :first-child").innerText
+        const ticketDate = ticket.querySelector(".date").innerText
+        
+        if(ticketDate == date && ticketArrival == airportArrival && ticketDeparture == airportDeparture) {
+            ticket.style.display = "flex"
+            find = true;
+            return;
+        }
+        ticket.style.display = "none"
+    })
+
+    if(!find) document.querySelector(".tickets-container h2").innerText = "We found no flight for you !"
 });
